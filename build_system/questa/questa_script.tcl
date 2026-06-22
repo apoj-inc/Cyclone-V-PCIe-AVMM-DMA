31c022f949153dde85be988fb062d479a55ffc72
FLStepCode <SerhanTalibov@yandex.ru>
2026-06-23 00:05:47 +0300

    fix: build_system is now a submodule

--- build_system/questa/questa_script.tcl
+++ build_system/questa/questa_script.tcl
@@ -8,7 +8,7 @@ set FILES_HEX_LIST [split $file_data "\n"]
 foreach hex $FILES_HEX_LIST {
     set hexfile_path [file tail $hex]
     if { [file exists $hexfile_path] != 1} {
-        file link $hexfile_path ../../../$hex
+        file link $hexfile_path ../../$hex
     }
 }
 
@@ -18,7 +18,7 @@ close $fp
 
 set FILES_RTL_LIST [split $file_data "\n"]
 for {set i 0} {$i < [llength $FILES_RTL_LIST]} {incr i} {
-    set appended ../../../[lindex $FILES_RTL_LIST $i]
+    set appended ../../[lindex $FILES_RTL_LIST $i]
     lset FILES_RTL_LIST $i $appended
 }
 
@@ -28,8 +28,19 @@ close $fp
 
 set FILES_TB_LIST [split $file_data "\n"]
 for {set i 0} {$i < [llength $FILES_TB_LIST]} {incr i} {
-    set appended ../../../[lindex $FILES_TB_LIST $i]
+    set appended ../../[lindex $FILES_TB_LIST $i]
     lset FILES_TB_LIST $i $appended
+}
+
+set fp [open $LIST_INC r]
+set file_data [read $fp]
+close $fp
+
+set INCDIRS_LIST [split $file_data "\n"]
+set INCDIRS_STRING {}
+for {set i 0} {$i < [llength $INCDIRS_LIST]} {incr i} {
+    set appended +../../[lindex $INCDIRS_LIST $i]
+    append INCDIRS_STRING ${appended}
 }
 
 set fp [open "files_rtl.lst" w+]
@@ -42,7 +53,7 @@ foreach rtl $FILES_TB_LIST {
 close $fp
 
 vlib work
-vlog -mfcu -f files_rtl.lst -suppress 13314
+vlog -mfcu +incdir${INCDIRS_STRING} -f files_rtl.lst -suppress 13314
 
 vsim -voptargs=+acc work.$TOPLEVEL +define+QUESTA
 if {[lsearch -exact $WildcardFilter Memory] >= 0} {
