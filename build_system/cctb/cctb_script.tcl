2e45073cdbae6d81bb1030966f2a48e2e33d2b33
FLStepCode <SerhanTalibov@yandex.ru>
2026-06-28 14:44:38 +0300

    fix: now it is a submodule

--- build_system/cctb/cctb_script.tcl
+++
@@ -1,9 +0,0 @@
-try {
-    if {[lsearch -exact $WildcardFilter Memory] >= 0} {
-        set WildcardFilter [lsearch -not -all -inline $WildcardFilter Memory];
-    }
-    log -r /*;
-    run -all;
-    exit;
-} on error {Error: Active logfile is not in simulation mode} {
-}
--- build_system/cctb/questa_after_vsim.do
+++
@@ -1,3 +0,0 @@
-if {[lsearch -exact $WildcardFilter Memory] >= 0} {
-    set WildcardFilter [lsearch -not -all -inline $WildcardFilter Memory]
-}
